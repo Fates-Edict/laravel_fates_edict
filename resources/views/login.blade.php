a193f246b909f2c6a2fd70fe648fab72e6fd5ea1
Mutaqin Yusuf <mutaqinyusuf73@gmail.com>
2025-04-11 00:33:12 +0700

    feat: login page

---
+++ resources/views/login.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Authentication - Teramedik Screening</title>
+    <link rel="stylesheet" href="/assets/css/bootstrap.min.css">
+</head>
+<body>
+    
+<div class="container pt-5">
+    <div class="row mt-5 justify-content-center">
+        <div class="card col-12 col-md-5 col-lg-4 py-4">
+            <div class="card-body">
+                <h3 class="text-center">Screening Teramedik</h3>
+                <p class="text-center">Authentication</p>
+
+                <form autocomplete="off" id="formLogin">
+                    <div class="mb-3">
+                        <label for="username" class="form-label">Username</label>
+                        <input type="text" class="form-control" id="username" autofocus>
+                    </div>
+                    <div class="mb-3">
+                        <label for="password" class="form-label">Password</label>
+                        <input type="password" class="form-control" id="password">
+                    </div>
+                    <div class="d-grid gap-2 col-12 mx-auto mb-3">
+                        <button class="btn btn-primary" type="submit">Submit</button>
+                    </div>
+                    <div class="text-center">
+                        <p>Programmed by <a href="https://www.linkedin.com/in/mutaqin-yusuf/" target="_blank">Mutaqin Yusuf</a></p>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script src="/assets/js/bootstrap.bundle.min.js"></script>
+<script src="/assets/js/jquery-3.7.1.min.js"></script>
+</body>
+</html>
